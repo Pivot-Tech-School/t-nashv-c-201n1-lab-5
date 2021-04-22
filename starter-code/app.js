28e6f92877dfd389198c72ebddc05cb44e0e0bce
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-04-22 16:15:52 -0500

    solved problem 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -77,7 +77,10 @@ console.log(sumAndMultiply(4,7,5));
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and 
+then returns an array where the first element is the sum of the numbers in the array, 
+and the second element is a string that EXACTLY follows this example 
+and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
@@ -86,12 +89,20 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    let totalArray = [];
+    let aPlusB = sum((sumArr[0]), (sumArr[1]))
+    aPlusB = aPlusB[0]
+    let finalSum = sum(aPlusB, sumArr[2])[0];
+    totalArray[0] = finalSum;
+    // string
+    totalArray[1] = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${finalSum} is their sum.`;
+    //return array
+return totalArray
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-
 // testSumArray(testArray);
+console.log(sumArray(testArray))
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
