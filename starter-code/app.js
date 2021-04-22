924d1ca4d7a13fa8c840e435cfe4372be0acdc11
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-04-22 14:52:15 -0500

    solved problem3

--- starter-code/app.js
+++ starter-code/app.js
@@ -33,20 +33,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-let theProduct = 5 * 9
+let theProduct = a * b
 let message = 'The product of ' + a + ' and ' + b + ' is ' + theProduct
 return [theProduct, message]
 }
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
-console.log(multiply(5,9))
+console.log(multiply(5,9));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where 
+the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  
+and the third and fourth elements are strings that EXACTLY follow this example 
+and use the values that were input into the function:
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
@@ -54,11 +58,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+let aPlusB = sum(a, b)[0]
+let sum2 = sum(aPlusB, c)[0]
 
+let aTimesB = multiply(a, b)[0]
+let product2 = multiply(aTimesB, c)[0]
+
+let message1 = a + ', ' + b + ', and ' + c + ' sum to ' + sum2 + '.'
+let message2 = 'The product of ' + a + ', ' + b + ', and ' + c + ' is ' + product2 + '.'
+return [sum2, product2, message1, message2]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
+console.log(sumAndMultiply(4,7,5));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
